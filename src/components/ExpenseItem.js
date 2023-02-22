87fecf3673525bbbff82586ebf94179ee2c3c728
shivam2399 <dubey.shivam2399@gmail.com>
2023-02-22 21:46:16 +0530

    passing location through props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -10,7 +10,7 @@ function ExpenseItem(props) {
           <div className="expense-item__description">
            <h2>{props.title}</h2>
            <div className="expense-item__price">{props.amount}</div>
-           {/* <div>{locationOfExpense}</div> */}
+           <div>{props.location}</div>
           </div>
         </div>
           
