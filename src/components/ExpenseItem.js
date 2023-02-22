ebad5bed8f9e11e08a9b6f9611a14343e9fe7765
shivam2399 <dubey.shivam2399@gmail.com>
2023-02-22 21:40:30 +0530

    passing data through props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 1, 22);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 290.45; 
-  const locationOfExpense = 'London' 
+function ExpenseItem(props) { 
+  // const locationOfExpense = 'London' 
     
     
   return (
         <div className="expense-item">
-          <div>{expenseDate.toISOString()}</div>
+          <div>{props.date.toISOString()}</div>
           <div className="expense-item__description">
-           <h2>{expenseTitle}</h2>
-           <div className="expense-item__price">{expenseAmount}</div>
-           <div>{locationOfExpense}</div>
+           <h2>{props.title}</h2>
+           <div className="expense-item__price">{props.amount}</div>
+           {/* <div>{locationOfExpense}</div> */}
           </div>
         </div>
           
