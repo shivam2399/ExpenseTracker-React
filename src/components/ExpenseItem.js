858a42ff5790d3173fb9756f1b8edc6d888e163e
shivam2399 <dubey.shivam2399@gmail.com>
2023-02-22 16:18:27 +0530

    adding loaction of expense

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
-    return (
+  const expenseDate = new Date(2023, 1, 22);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 290.45; 
+  const locationOfExpense = 'London' 
+    
+    
+  return (
         <div className="expense-item">
-          <div>February 22nd 2023</div>
+          <div>{expenseDate.toISOString()}</div>
           <div className="expense-item__description">
-           <h2>Car Insurance</h2>
-           <div className="expense-item__price">$294.67</div>
+           <h2>{expenseTitle}</h2>
+           <div className="expense-item__price">{expenseAmount}</div>
+           <div>{locationOfExpense}</div>
           </div>
         </div>
           
