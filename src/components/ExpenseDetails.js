e84a020affbffaceb53a26a689bba5d5ec137e89
shivam2399 <dubey.shivam2399@gmail.com>
2023-02-28 11:35:27 +0530

    Creating separate components for date and details

---
+++ src/components/ExpenseDetails.js
@@ -0,0 +1,13 @@
+
+
+function ExpenseDetails(props) {
+    return (
+        <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+        <div>{props.location}</div>
+        </div>
+    )
+}
+
+export default ExpenseDetails;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,14 @@
+import ExpenseDetails from './ExpenseDetails';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) { 
-  // const locationOfExpense = 'London' 
-    
-    
   return (
         <div className="expense-item">
-          <div>{props.date.toISOString()}</div>
-          <div className="expense-item__description">
-           <h2>{props.title}</h2>
-           <div className="expense-item__price">{props.amount}</div>
-           <div>{props.location}</div>
-          </div>
+          <ExpenseDate date = {props.date}/>
+          <ExpenseDetails amount = {props.amount} />
+          <ExpenseDetails title = {props.title} />
+          <ExpenseDetails location = {props.location} />
         </div>
           
         
