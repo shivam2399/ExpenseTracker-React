9f03bae172fe49816ca6247d6318155264a0fcb9
shivam2399 <dubey.shivam2399@gmail.com>
2023-03-02 13:03:17 +0530

    delete expense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,18 @@ import './ExpenseItem.css'
 
 
 const ExpenseItem = (props) => { 
+  
+  const clickHandler = (e) => {
+    e.target.parentNode.remove()
+  }
+
   return (
         <Card className="expense-item">
           <ExpenseDate date = {props.date}/>
           <ExpenseDetails amount = {props.amount} />
           <ExpenseDetails title = {props.title} />
           <ExpenseDetails location = {props.location} />
+          <button onClick={clickHandler}>Delete Expense</button>
         </Card>
           
         
